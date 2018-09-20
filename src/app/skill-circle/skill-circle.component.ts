687f0cdcd00b194d080e26bb6f089c0ed828d210
Steve Schrab <steve.schrab@thomsonreuters.com>
2018-09-20 15:35:05 -0500

    Add skill-circle transition

--- src/app/skill-circle/skill-circle.component.ts
+++ src/app/skill-circle/skill-circle.component.ts
@@ -12,27 +12,42 @@ export class SkillCircleComponent implements OnInit {
   @Input() skill: string;
   @Input() percentage: number;
 
-  private circle: SVGElement;
+  private circle: NodeListOf<SVGElement>;
   private radius: number;
   private circumference: number;
 
   constructor(public el: ElementRef) { }
 
   ngOnInit() {
-    this.circle = this.el.nativeElement.querySelector('.skill-circle--amount');
-    this.radius = parseFloat(this.circle.getAttribute('r'));
-    this.circumference = 2 * Math.PI * this.radius;
+    const circle: NodeListOf<SVGElement> = this.el.nativeElement.querySelectorAll('.skill-circle--amount');
+    const circleLow: SVGElement = this.el.nativeElement.querySelector('.skill-circle--amount.low');
+    const circleMedium: SVGElement = this.el.nativeElement.querySelector('.skill-circle--amount.medium');
+    const circleHigh: SVGElement = this.el.nativeElement.querySelector('.skill-circle--amount.high');
+    const radius: number = parseFloat(this.el.nativeElement.querySelector('.skill-circle--amount').getAttribute('r'));
+    const circumference: number = 2 * Math.PI * radius;
 
     const timer = offSetTime * 1000;
+    const circleLength = circle.length;
 
-    // Set the initial strokeDashArray and strokeDashoffset to the circumference
-    this.circle.style.strokeDasharray = `${this.circumference}`;
-    this.circle.style.strokeDashoffset = `${this.circumference}`;
+    for (let x = 0; x < circleLength; x++) {
+      // Set the initial strokeDashArray and strokeDashoffset to the circumference
+      circle[x].style.strokeDasharray = `${circumference}`;
+      circle[x].style.strokeDashoffset = `${circumference}`;
 
-    // Set strokeDashoffset to a percentage of the circumference
-    setTimeout(() => {
-      this.circle.style.strokeDashoffset = `${this.circumference * (1 - (this.percentage * .01))}`;
-    }, timer);
+      setTimeout(() => {
+        // Set strokeDashoffset to a percentage of the circumference
+        circle[x].style.strokeDashoffset = `${circumference * (1 - (this.percentage * .01))}`;
+
+        console.log((((this.percentage * 50) / 100) - 100));
+        if (this.percentage >= 50) {
+          circleLow.style.opacity = `${0}`;
+          circleMedium.style.opacity = `${(100 - this.percentage) * .02}`;
+        } else {
+          circleHigh.style.opacity = `${0}`;
+          circleLow.style.opacity = `${(100 - ((this.percentage * 50) / 100)) * .01}`;
+        }
+      }, timer);
+    }
 
     // Increment timer for each instance of the component
     offSetTime++;
